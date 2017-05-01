e5d89d353cc8f5db6dcbf0916eb0f6f7edc7fee5
xKxAxKx <pekepooooong@gmail.com>
2017-05-01 10:30:54 +0900

    [update]ルーティングの修正

--- ng2app/src/app/app-routing.module.ts
+++ ng2app/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { TodoListComponent }    from './components/todo-list.component';
-import { TodoComponent }    from './components/todo.component';
 
 const routes: Routes = [
   { path: '',  component: TodoListComponent }
