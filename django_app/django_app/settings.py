85d36cb74d9981a3ce59bea08e2eee987647a5eb
xKxAxKx <pekepooooong@gmail.com>
2017-05-02 13:49:19 +0900

    [fix]settings.pyを戻し
    結局JWT使うことにする

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -131,8 +131,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
      'DEFAULT_PAGINATION_CLASS': (
-        'rest_framework.pagination.LimitOffsetPagination',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.pagination.LimitOffsetPagination'
     )
 }
