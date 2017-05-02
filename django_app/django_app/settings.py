f306b3fc2af93e520adaae771d0c898fb4929612
xKxAxKx <pekepooooong@gmail.com>
2017-05-02 12:40:16 +0900

    [update]settings.pyに認証のclassを追加

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -130,5 +130,9 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
-     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+     'DEFAULT_PAGINATION_CLASS': (
+        'rest_framework.pagination.LimitOffsetPagination',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
 }
