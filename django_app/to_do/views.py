97452cf38b6e99a052dd511cdb14f2e32dc7641a
xKxAxKx <pekepooooong@gmail.com>
2017-05-10 18:48:21 +0900

    [update]views.pyに未認証状態でのフィルタリング追加

--- django_app/to_do/views.py
+++ django_app/to_do/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 import django_filters
 from rest_framework import viewsets, filters
+from rest_framework.decorators import api_view
 
 from .models import Todo
 from .serializer import TodoSerializer
 
-from rest_framework.decorators import api_view
+from rest_framework import permissions
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all().order_by('-created_at')
     serializer_class = TodoSerializer
+
+    #フィルタの追加
+    filter_fields = ('publishing',)
+    #認証追加 CURD全てに認証掛けたい場合は (permissions.IsAuthenticated,)にする
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
