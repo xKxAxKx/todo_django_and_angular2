18d5509f2127407b1b6c7baa740246d09053af34
xKxAxKx <pekepooooong@gmail.com>
2017-05-10 18:37:41 +0900

    [update]userアプリケーションが不要っぽいので削除

--- django_app/users/urls.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import routers
-from .views import UserViewSet
-
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
--- django_app/users/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets, filters
-
-from django.contrib.auth.models import User
-from .serializer import UserSerializer
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
